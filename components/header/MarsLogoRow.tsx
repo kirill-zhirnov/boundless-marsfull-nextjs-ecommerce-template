68ea173b2515fb0290763550005e0a0b13280d7b
Artyom Palevich <hello@apalevich.ru>
2022-07-30 18:00:53 +0300

    Deleted testing files

--- components/header/MarsLogoRow.tsx
+++ components/header/MarsLogoRow.tsx
@@ -3,7 +3,7 @@ import HeaderCart from '../cart/HeaderCart';
 import {useAppDispatch, useAppSelector} from '../../hooks/redux';
 import {MouseEvent} from 'react';
 import {setIsOpened} from '../../redux/reducers/asideMenu';
-import logoImg from '../../assets/Logo_mockup_vert.jpg';
+// import logoImg from '../../assets/logo.svg';
 import clsx from 'clsx';
 import {RootState} from '../../redux/store';
 import {faShoppingCart} from '@fortawesome/free-solid-svg-icons/faShoppingCart';
@@ -24,8 +24,8 @@ export default function MarsLogoRow() {
 				<div className={'mars-logo-row__logo-wrapper'}>
 					<Link href={'/'}>
 						<a className={'mars-logo-row__logo'}>
-							{/* <span>Brand Shop</span> */}
-							<img src={logoImg.src} alt={'Brand Shop'} />
+							<span>Brand Shop</span>
+							{/* <img src={logoImg.src} alt={'Brand Shop'} /> */}
 						</a>
 					</Link>
 				</div>
